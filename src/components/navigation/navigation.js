1b136c9e6d033526ff20d5c9085e8ca827173816
Victoria-Butrym <victoria-butrym@tut.by>
2020-02-18 22:22:55 +0300

    feat: add our team page

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav } from "react-bootstrap"
 
 const Navigation = () => (
   <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
@@ -9,12 +9,13 @@ const Navigation = () => (
       <Nav className="custom-mr_left">
         <Nav.Link href="/temp/main">Main</Nav.Link>
         <Nav.Link href="#ho">Filmmakers</Nav.Link>
-        <Nav.Link href="#hm">Our team</Nav.Link>
+        {/* <Nav.Link href="#hm">Our team</Nav.Link> */}
+        <Nav.Link href="/temp/author">Our team</Nav.Link>
         <Nav.Link href="#ha">Worklog</Nav.Link>
         <Nav.Link href="#hq">Styleguide</Nav.Link>
       </Nav>
     </Navbar.Collapse>
-  </Navbar >
+  </Navbar>
 )
 
 export default Navigation
