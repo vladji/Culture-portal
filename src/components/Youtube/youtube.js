fdbd9968780f9a526d8d9772f4178f82cc15e57d
Victoria-Butrym <victoria-butrym@tut.by>
2020-02-19 20:48:20 +0300

    feat: create Youtube component

---
+++ src/components/Youtube/youtube.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+
+import YouTube from "react-youtube"
+
+class Youtube extends Component {
+  onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo()
+  }
+
+  render() {
+    const opts = {
+      height: "390",
+      width: "640",
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    }
+
+    const { videoId } = this.props
+
+    return <YouTube videoId={videoId} opts={opts} onReady={this.onReady} />
+  }
+}
+
+Youtube.propTypes = {
+  videoId: PropTypes.string.isRequired,
+  onReady: PropTypes.func,
+  opts: PropTypes.object,
+}
+
+Youtube.defaultProps = {
+    videoId: '',
+    onReady: () => {},
+    opts: {}
+}
+
+export default Youtube
