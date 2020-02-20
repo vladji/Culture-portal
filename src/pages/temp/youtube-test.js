c1610eea6b42673839f31fd95a627b8a58feed59
Victoria-Butrym <victoria-butrym@tut.by>
2020-02-20 21:16:47 +0300

    feat: add testing components to youtube-test page

--- src/pages/temp/youtube-test.js
+++ src/pages/temp/youtube-test.js
@@ -1,13 +1,37 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import YoutubeModal from "../../components/YoutubeModal/youtubeModal"
 
+const shortUrl = "8Lzi80kxo4E"
+const fullUrl = "https://www.youtube.com/watch?v=kcrOAF473LM"
+
 const YoutubeTestPage = () => {
   return (
-    <>
-      <YoutubeModal videoId="8Lzi80kxo4E" />
-      <YoutubeModal videoId="https://www.youtube.com/watch?v=kcrOAF473LM" />
-    </>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h3>Youtube component testing page</h3>
+      <div
+        style={{
+          marginTop: "2rem",
+          marginBottom: "2rem",
+          width: "10%",
+          height: "30px",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <YoutubeModal videoId={shortUrl} />
+        <YoutubeModal videoId={fullUrl} />
+      </div>
+      <Link to="/">Back to homepage</Link>
+    </div>
   )
 }
 export default YoutubeTestPage
