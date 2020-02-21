05d44b745e54ede52076aa0d5e0b955286274d92
OksanaShuptar <angels_ua@ukr.net>
2020-02-21 22:26:58 +0200

    feat: add components to storybook

---
+++ stories/Card.stories.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+
+import '../src/styles/bootstrap.min.css'
+import '../src/pages/temp/styleguide.css'
+
+export default {
+  title: 'Card',
+}
+
+export const directorsCard = () => {
+  return (
+    <Container>
+      <Row>
+        <Col lg={4}>
+          <Card>
+            <Card.Img variant="top" src="https://via.placeholder.com/150" />
+            <Card.Body className="card-body">
+              <Card.Title className="card-title">
+                Card Title
+              </Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary" className="btn btn-primary">
+                Go somewhere
+              </Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
---
+++ stories/Color.stories.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import '../src/pages/temp/styleguide.css'
+
+export default {
+  title: 'Colors',
+};
+
+export const primary = () => {
+  return (
+    <div style={{backgroundColor: '#010000'}} className="color-card">
+        <span className="color-value">#010000</span>
+    </div>
+  )
+}
+
+export const secondary = () => {
+  return (
+    <div style={{backgroundColor: '#1a1a1a'}} className="color-card">
+      <span className="color-value">#1a1a1a</span>
+    </div>
+  )
+}
+
+export const green = () => {
+  return (
+    <div style={{backgroundColor: '#4bbf73'}} className="color-card">
+      <span className="color-value">#4bbf73</span>
+    </div>
+  )
+}
+
+export const red = () => {
+  return (
+    <div style={{backgroundColor: '#d9534f'}} className="color-card">
+      <span className="color-value">#d9534f</span>
+    </div>
+  )
+}
+
+export const grey = () => {
+  return (
+    <div style={{backgroundColor: '#f7f7f9'}} className="color-card">
+      <span className="color-value">#f7f7f9</span>
+    </div>
+  )
+}
