ab514ae38b44cd272e4898f768a604f9ab0f34e8
Victoria-Butrym <victoria-butrym@tut.by>
2020-02-27 21:27:06 +0300

    fix: fix dropdown items on mobile

--- src/components/LangMenu/LangMenu.js
+++ src/components/LangMenu/LangMenu.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { DropdownButton, Dropdown } from 'react-bootstrap';
 import { onLinkClickHandler } from "../../utils/language"
 
+import './LangMenu.css'
+
 const LangMenu = ({ location, lang }) => {
 
 
@@ -11,9 +13,9 @@ const LangMenu = ({ location, lang }) => {
     <DropdownButton alignRight
       id="dropdown-menu-align-right"
       title={lang}>
-      <Dropdown.Item as="button" onClick={()=>onLinkClickHandler('/en/', location.pathname, lang)} data-lang="en">EN</Dropdown.Item>
-      <Dropdown.Item as="button" onClick={()=>onLinkClickHandler('', location.pathname, lang)} data-lang="be">BY</Dropdown.Item>
-      <Dropdown.Item as="button" onClick={()=>onLinkClickHandler('/ru/', location.pathname, lang)} data-lang="ru">RU</Dropdown.Item>
+      <Dropdown.Item as="button" onClick={() => onLinkClickHandler('/en/', location.pathname, lang)} data-lang="en">EN</Dropdown.Item>
+      <Dropdown.Item as="button" onClick={() => onLinkClickHandler('', location.pathname, lang)} data-lang="be">BY</Dropdown.Item>
+      <Dropdown.Item as="button" onClick={() => onLinkClickHandler('/ru/', location.pathname, lang)} data-lang="ru">RU</Dropdown.Item>
     </DropdownButton>
   )
 }
