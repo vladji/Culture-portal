ceee02979a34483e5b484e52f846801e6ed6c79b
Victoria-Butrym <victoria-butrym@tut.by>
2020-02-27 21:51:52 +0300

    fix: add menu @media

--- src/components/LangMenu/LangMenu.js
+++ src/components/LangMenu/LangMenu.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { DropdownButton, Dropdown } from 'react-bootstrap';
 import { onLinkClickHandler } from "../../utils/language"
 
-import './LangMenu.css'
-
 const LangMenu = ({ location, lang }) => {
 
 
@@ -13,9 +11,9 @@ const LangMenu = ({ location, lang }) => {
     <DropdownButton alignRight
       id="dropdown-menu-align-right"
       title={lang}>
-      <Dropdown.Item as="button" onClick={() => onLinkClickHandler('/en/', location.pathname, lang)} data-lang="en">EN</Dropdown.Item>
-      <Dropdown.Item as="button" onClick={() => onLinkClickHandler('', location.pathname, lang)} data-lang="be">BY</Dropdown.Item>
-      <Dropdown.Item as="button" onClick={() => onLinkClickHandler('/ru/', location.pathname, lang)} data-lang="ru">RU</Dropdown.Item>
+      <Dropdown.Item as="button" onClick={()=>onLinkClickHandler('/en/', location.pathname, lang)} data-lang="en">EN</Dropdown.Item>
+      <Dropdown.Item as="button" onClick={()=>onLinkClickHandler('', location.pathname, lang)} data-lang="be">BY</Dropdown.Item>
+      <Dropdown.Item as="button" onClick={()=>onLinkClickHandler('/ru/', location.pathname, lang)} data-lang="ru">RU</Dropdown.Item>
     </DropdownButton>
   )
 }
