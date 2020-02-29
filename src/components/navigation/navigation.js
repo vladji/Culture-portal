556b6390fc5395608d091fcf02a52cb0da37bfa0
Victoria-Butrym <victoria-butrym@tut.by>
2020-02-29 15:16:24 +0300

    fix: logo translation

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -4,37 +4,57 @@ import { Navbar, Nav } from "react-bootstrap";
 import LangMenu from "../LangMenu/LangMenu";
 import Logo from '../Logo/Logo'
 import { getLangPath } from "../../utils/language"
+import { getFields } from "../../utils/fields"
 
 
 const Navigation = ({ location }) => {
-  const { markdownRemark } = useStaticQuery(graphql`
-{
-  markdownRemark(frontmatter: {type: {eq: "navigation"}}) {
-    frontmatter {
-      navigations {
-        name
-        navigation {
-          be
-          en
-          ru
+  const query = useStaticQuery(graphql`
+  query {
+    logo: markdownRemark(
+      frontmatter: { type: { eq: "page" }, name: { eq: "main" } }
+    ) {
+      frontmatter {
+        fields {
+          fieldName
+          fieldData {
+            ru
+            be
+            en
+          }
+        }
+      }
+    }
+    navigation: markdownRemark(
+      frontmatter: { type: { eq: "navigation" } }
+    ) {
+      frontmatter {
+        navigations {
+          name
+          navigation {
+            ru
+            be
+            en
+          }
         }
       }
     }
   }
-}
-
   `)
-  const navigationData = markdownRemark.frontmatter.navigations;
-  const getLang = ()=>{
+
+  const navigationData = query.navigation.frontmatter.navigations;
+  const getLang = () => {
     switch (true) {
-      case (location.pathname.indexOf('/ru/') !== -1) : return 'ru';
-      case (location.pathname.indexOf('/en/') !== -1) : return 'en';
+      case (location.pathname.indexOf('/ru/') !== -1): return 'ru';
+      case (location.pathname.indexOf('/en/') !== -1): return 'en';
       default: return 'be';
     }
   }
   const lang = getLang();
-  const getLabel = (name)=>{
-    return navigationData.find(el=>el.name===name).navigation[lang];
+  const logoSource = query.logo.frontmatter.fields;
+  const logoTitle = getFields("logo", logoSource, lang)
+
+  const getLabel = (name) => {
+    return navigationData.find(el => el.name === name).navigation[lang];
   }
 
   const onLinkClickHandler = (e, path) => {
@@ -45,7 +65,7 @@ const Navigation = ({ location }) => {
   return (
     <Navbar collapseOnSelect expand="lg" className="navbar navbar-dark bg-primary">
       <Navbar.Brand href="/" onClick={(e) => onLinkClickHandler(e, '')} className="navbar-brand">
-        <Logo />
+        <Logo title={logoTitle} />
       </Navbar.Brand>
 
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -56,7 +76,7 @@ const Navigation = ({ location }) => {
           <Nav.Link href="/" onClick={(e) => onLinkClickHandler(e, 'developers/')}>{getLabel('ourTeam')}</Nav.Link>
           <Nav.Link href="/" onClick={(e) => onLinkClickHandler(e, 'worklog/')}>{getLabel('worklog')}</Nav.Link>
           <Nav.Link href="/" onClick={(e) => onLinkClickHandler(e, 'styleguide/')}>{getLabel('styleguide')}</Nav.Link>
-          <LangMenu location={location} lang={lang}/>
+          <LangMenu location={location} lang={lang} />
         </Nav>
       </Navbar.Collapse>
     </Navbar>
